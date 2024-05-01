5e9f3dcdb527a3aeaba4ecbcc3ae3f612d487273
Soham Adwani <sohamthenerd@gmail.com>
2024-05-01 10:20:02 +0700

    added for mac

--- .wezterm.lua
+++ .wezterm.lua
@@ -13,6 +13,8 @@ config.use_fancy_tab_bar = false
 -- config.color_scheme = 'Catppuccin Mocha'
 config.color_scheme = 'zenbones_dark'
 config.font = wezterm.font 'JetBrains Mono'
+config.window_decorations = "RESIZE"
+config.font_size = 16.0
 -- config.font = wezterm.font 'CommitMono Nerd Font'
 
 config.warn_about_missing_glyphs = false
