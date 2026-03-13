9304a3a3cc45c34ddf56e4d7733e96eddbf16bf6
Soham Adwani <sohamthenerd@gmail.com>
2026-03-13 11:53:18 +0700

    new desktop changes and split out desktop and laptop configs

--- rofi/power-menu.sh
+++ rofi/power-menu.sh
@@ -9,8 +9,8 @@ options="󰌾  lock
 choice=$(echo "$options" | rofi -dmenu -p "Power Menu")
 
 case $choice in
-  *lock) killall vesktop || true && swaylock -f && sleep 1 && hyprctl dispatch dpms off ;;
-  *suspend) swaylock -f && systemctl suspend ;;
+  *lock) killall Discord || true && hyprlock && sleep 1 && hyprctl dispatch dpms off ;;
+  *suspend) hyprlock && systemctl suspend ;;
   *logout) hyprctl dispatch exit 0 ;;
   *shutdown) systemctl poweroff ;;
   *reboot) systemctl reboot ;;
